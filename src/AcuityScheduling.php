ee25f2894bb95e855dc0203350ba866bcd9e2ea9
Quik Clinik <quikclinik@gmail.com>
2016-08-07 13:21:08 -0500

    Add classes to namespace. PCS fixes. Set scope globally in Oauth. Ensure endpoint matches scope in oauth

--- src/AcuityScheduling.php
+++ src/AcuityScheduling.php
@@ -1,168 +1,178 @@
 <?php
+namespace Acuity;
 
-class AcuityScheduling {
+class Scheduling
+{
 
-	protected $base = 'https://acuityscheduling.com';
-	protected $userId = null;
-	protected $apiKey = null;
-	protected $lastStatusCode = null;
+    protected $base = 'https://acuityscheduling.com';
+    protected $userId = null;
+    protected $apiKey = null;
+    protected $lastStatusCode = null;
 
-	public function __construct($options) {
-		$this->apiKey = $options['apiKey'];
-		$this->userId = $options['userId'];
-		$this->base		= $options['base'] ? $options['base'] : $this->base;
-	}
+    public function __construct($options)
+    {
+        $this->apiKey = $options['apiKey'];
+        $this->userId = $options['userId'];
+        $this->base   = isset($options['base']) ? $options['base'] : $this->base;
+    }
 
-	public function request($path, $options = array()) {
-		$path = is_string($path) ? $path : '';
-		$url = $this->base.'/api/v1'.($path[0] == '/' ? '' : '/').$path;
-		return $this->_request($url, array_merge($options, array(
-			'json' => true,
-			'username' => $this->userId,
-			'password' => $this->apiKey
-		)));
-	}
+    public function request($path, $options = array())
+    {
+        $path = is_string($path) ? $path : '';
+        $url = $this->base.'/api/v1'.($path[0] == '/' ? '' : '/').$path;
+        return $this->__request($url, array_merge($options, array(
+            'json' => true,
+            'username' => $this->userId,
+            'password' => $this->apiKey
+        )));
+    }
 
-	public function getLastRequestInfo() {
-		return array(
-			'status_code' => $this->lastStatusCode
-		);
-	}
+    public function getLastRequestInfo()
+    {
+        return array(
+            'status_code' => $this->lastStatusCode
+        );
+    }
 
-	/**
-	 * Request helper.
-	 */
-	protected function _request($url, $options) {
+    /**
+     * Request helper.
+     */
+    protected function __request($url, $options)
+    {
 
-		// Set defaults:
-		$options = array_merge(array(
-			'method'   => 'GET',
-			'username' => null,
-			'password' => null,
-			'json'     => false,
-			'headers'  => array(),
-			'data'     => null,
-			'query'    => null
-		), $options);
-		$method = $options['method'];
-		$headers = $options['headers'];
-		$query = $options['query'];
-		$data = $options['data'];
-		$json = $options['json'] === true;
+        // Set defaults:
+        $options = array_merge(array(
+            'method'   => 'GET',
+            'username' => null,
+            'password' => null,
+            'json'     => false,
+            'headers'  => array(),
+            'data'     => null,
+            'query'    => null
+        ), $options);
+        $method = $options['method'];
+        $headers = $options['headers'];
+        $query = $options['query'];
+        $data = $options['data'];
+        $json = $options['json'] === true;
 
-		// Data type:
-		if ($data) {
-			if ($json) {
-				$headers['Content-Type'] = 'application/json';
-				$data = is_string($data) ? $data : json_encode($data);
-			} else {
-				$headers['Content-Type'] = 'application/x-www-form-urlencoded';
-				$data = is_string($data) ? $data : http_build_query($data);
-			}
-		}
-		if ($query && is_array($query)) {
-			$url .= '?'.http_build_query($query);
-		}
+        // Data type:
+        if ($data) {
+            if ($json) {
+                $headers['Content-Type'] = 'application/json';
+                $data = is_string($data) ? $data : json_encode($data);
+            } else {
+                $headers['Content-Type'] = 'application/x-www-form-urlencoded';
+                $data = is_string($data) ? $data : http_build_query($data);
+            }
+        }
+        if ($query && is_array($query)) {
+            $url .= '?'.http_build_query($query);
+        }
 
-		// Headers:
-		$header = array();
-		foreach ($headers as $key => $value) {
-			$header[] = "{$key}: {$value}";
-		}
+        // Headers:
+        $header = array();
+        foreach ($headers as $key => $value) {
+            $header[] = "{$key}: {$value}";
+        }
 
-		// Config:
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_HEADER, 1);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_USERAGENT, 'AcuityScheduling-php');
-		if ($options['username'] && $options['password']) {
-			curl_setopt($ch, CURLOPT_USERPWD, "{$options['username']}:{$options['password']}");
-		}
+        // Config:
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'AcuityScheduling-php');
+        if ($options['username'] && $options['password']) {
+            curl_setopt($ch, CURLOPT_USERPWD, "{$options['username']}:{$options['password']}");
+        }
 
-		// Methods:
-		switch ($method) {
-		case 'GET':
-			break;
-		case 'POST':
-			curl_setopt($ch, CURLOPT_POST, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-			break;
-		case 'PUT':
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-		case 'DELETE':
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-		default:
-			throw new Exception("Invalid request method ({$method})");
-		}
+        // Methods:
+        switch ($method) {
+            case 'GET':
+                break;
+            case 'POST':
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                break;
+            case 'PUT':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+                // Set options for PUT
+            case 'DELETE':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+                // Set additional options for DELETE and PUT
+            default:
+                throw new Exception("Invalid request method ({$method})");
+        }
 
-		// Request:
-		$result = curl_exec($ch);
-		$this->lastStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		$headerLength = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-		if ($result) {
-			$body = substr($result, $headerLength);
-			$body = $json ? json_decode($body, true) : $body;
-		} else {
-			$body = [
-				'message' => curl_error($ch),
-				'error'   => 'curl_error_'.curl_errno($ch)
-			];
-		}
+        // Request:
+        $result = curl_exec($ch);
+        $this->lastStatusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $headerLength = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+        if ($result) {
+            $body = substr($result, $headerLength);
+            $body = $json ? json_decode($body, true) : $body;
+        } else {
+            $body = [
+                'message' => curl_error($ch),
+                'error'   => 'curl_error_'.curl_errno($ch)
+            ];
+        }
 
-		return $body;
-	}
+        return $body;
+    }
 
-	/**
-	 * Verify a message signature using your API key.
-	 */
-	public static function verifyMessageSignature($secret, $body = null, $signature = null) {
+    /**
+     * Verify a message signature using your API key.
+     */
+    public static function verifyMessageSignature($secret, $body = null, $signature = null)
+    {
 
-		// Compute hash of message using shared secret:
-		$body = is_null($body) ? file_get_contents('php://input') : $body;
-		$hash = base64_encode(hash_hmac('sha256', $body, $secret, true));
+        // Compute hash of message using shared secret:
+        $body = is_null($body) ? file_get_contents('php://input') : $body;
+        $hash = base64_encode(hash_hmac('sha256', $body, $secret, true));
 
-		// Compare hash to the signature:
-		$signature = is_null($signature) ? $_SERVER['HTTP_X_ACUITY_SIGNATURE'] : $signature;
-		if ($hash !== $signature) {
-			throw new Exception('This message was forged!');
-		}
-	}
+        // Compare hash to the signature:
+        $signature = is_null($signature) ? $_SERVER['HTTP_X_ACUITY_SIGNATURE'] : $signature;
+        if ($hash !== $signature) {
+            throw new Exception('This message was forged!');
+        }
+    }
 
-	/**
-	 * Generate embed code for $owner.
-	 *
-	 *
-	 * @param $owner  The owner's id.
-	 * @param $options  Additional options.
-	 *	- width  Iframe width
-	 *	- height  Iframe height
-	 *	- query  Query string arguments
-	 */
-	public static function getEmbedCode($owner, $options = array())
-	{
-		$query = array(
-			'owner' => $owner
-		);
-		$options = array_merge(array(
-			'height' => '800',
-			'width' => '100%'
-		), $options);
+    /**
+     * Generate embed code for $owner.
+     *
+     *
+     * @param $owner  The owner's id.
+     * @param $options  Additional options.
+     *  - width  Iframe width
+     *  - height  Iframe height
+     *  - query  Query string arguments
+     */
+    public static function getEmbedCode($owner, $options = array())
+    {
+        $query = array(
+            'owner' => $owner
+        );
+        $options = array_merge(array(
+            'height' => '800',
+            'width' => '100%'
+        ), $options);
 
-		// Encode options:
-		foreach ($options as $key => $option) {
-			if ($key === 'query') {
-				$query = array_merge($query, $option);
-			} else {
-				$options[$key] = htmlspecialchars($option);
-			}
-		}
-		$query = http_build_query($query);
+        // Encode options:
+        foreach ($options as $key => $option) {
+            if ($key === 'query') {
+                $query = array_merge($query, $option);
+            } else {
+                $options[$key] = htmlspecialchars($option);
+            }
+        }
+        $query = http_build_query($query);
 
-		return
-			"<iframe src=\"https://app.acuityscheduling.com/schedule.php?{$query}\" width=\"{$options['width']}\" height=\"{$options['height']}\" frameBorder=\"0\"></iframe>".
-			'<script src="https://d3gxy7nm8y4yjr.cloudfront.net/js/embed.js" type="text/javascript"></script>';
-	}
+        return
+            "<iframe src=\"https://app.acuityscheduling.com/schedule.php?{$query}\"
+            width=\"{$options['width']}\" height=\"{$options['height']}\" frameBorder=\"0\"></iframe>".
+            '<script src="https://d3gxy7nm8y4yjr.cloudfront.net/js/embed.js" type="text/javascript"></script>';
+    }
 }
--- src/AcuitySchedulingOAuth.php
+++ src/AcuitySchedulingOAuth.php
@@ -1,81 +1,86 @@
 <?php
+namespace Acuity;
 
-require_once(__DIR__.DIRECTORY_SEPARATOR.'AcuityScheduling.php');
+//require_once(__DIR__.DIRECTORY_SEPARATOR.'AcuityScheduling.php');
 
-class AcuitySchedulingOAuth extends AcuityScheduling {
+class OAuth extends Scheduling
+{
 
-	protected $accessToken;
-	protected $clientId;
-	protected $clientSecret;
-	protected $redirectUri;
+    protected $accessToken;
+    protected $clientId;
+    protected $clientSecret;
+    protected $redirectUri;
+    protected $scope; //Set scope globally
 
-	public function __construct($options) {
-		$this->accessToken  = $options['accessToken'];
-		$this->clientId     = $options['clientId'];
-		$this->clientSecret = $options['clientSecret'];
-		$this->redirectUri  = $options['redirectUri'];
-		$this->base         = $options['base'] ? $options['base'] : $this->base;
-	}
+    public function __construct($options)
+    {
+        if (!isset($options['scope'])) {
+            trigger_error('Missing `scope` parameter.', E_USER_ERROR);
+        }
+        $this->accessToken  = isset($options['accessToken']) ? $options['accessToken'] : "";
+        $this->clientId     = $options['clientId'];
+        $this->clientSecret = $options['clientSecret'];
+        $this->redirectUri  = $options['redirectUri'];
+        $this->base         = isset($options['base']) ? $options['base'] : $this->base;
+    }
 
-	public function getAuthorizeUrl($params)
-	{
-		if (!isset($params['scope'])) {
-			trigger_error('Missing `scope` parameter.', E_USER_ERROR);
-		}
+    public function getAuthorizeUrl($params = [])
+    {
+        $query = array(
+            'response_type' => 'code',
+            'scope'         => $this->scope,
+            'client_id'     => $this->clientId,
+            'redirect_uri'  => $this->redirectUri
+        );
 
-		$query = array(
-			'response_type' => 'code',
-			'scope'         => $params['scope'],
-			'client_id'     => $this->clientId,
-			'redirect_uri'  => $this->redirectUri
-		);
+        if (isset($params['state'])) {
+            $query['state'] = $params['state'];
+        }
 
-		if ($params['state']) {
-			$query['state'] = $params['state'];
-		}
+        return $this->base.'/oauth2/authorize'.'?'.http_build_query($query);
+    }
 
-		return $this->base.'/oauth2/authorize'.'?'.http_build_query($query);
-	}
+    public function authorizeRedirect($params = [])
+    {
+        header('Location: '.$this->getAuthorizeUrl($params));
+    }
 
-	public function authorizeRedirect($params)
-	{
-		header('Location: '.$this->getAuthorizeUrl($params));
-	}
+    public function requestAccessToken($code)
+    {
+        $data = array(
+            'grant_type'    => 'authorization_code',
+            'code'          => $code,
+            'client_id'     => $this->clientId,
+            'client_secret' => $this->clientSecret,
+            'redirect_uri'  => $this->redirectUri
+        );
 
-	public function requestAccessToken($code)
-	{
-		$data = array(
-			'grant_type'    => 'authorization_code',
-			'code'          => $code,
-			'client_id'     => $this->clientId,
-			'client_secret' => $this->clientSecret,
-			'redirect_uri'  => $this->redirectUri
-		);
+        $body = $this->__request($this->base.'/oauth2/token', array(
+            'data' => $data,
+            'method' => 'POST'
+        ));
+        $body = json_decode($body, true);
+        if ($body['access_token']) {
+            $this->accessToken = $body['access_token'];
+        }
 
-		$body = $this->_request($this->base.'/oauth2/token', array(
-			'data' => $data,
-			'method' => 'POST'
-		));
-		$body = json_decode($body, true);
-		if ($body['access_token']) {
-			$this->accessToken = $body['access_token'];
-		}
+        return $body;
+    }
 
-		return $body;
-	}
+    public function isConnected()
+    {
+        return $this->accessToken ? true : false;
+    }
 
-	public function isConnected() {
-		return $this->accessToken ? true : false;
-	}
-
-	public function request($path, $options = array()) {
-		$url = $this->base.'/api/v1/'.$path;
-		return $this->_request($url, array_merge($options, array(
-			'json' => true,
-			'headers' => array(
-				'Authorization' => 'Bearer '.$this->accessToken
-			)
-		)));
-	}
-
+    public function request($path, $options = array())
+    {
+        // Ensure call made only to endpoint matching scope
+        $url = $this->base.'/'.str_replace("-", "/", $this->scope).'/'.$path;
+        return $this->__request($url, array_merge($options, array(
+            'json' => true,
+            'headers' => array(
+                'Authorization' => 'Bearer '.$this->accessToken
+            )
+        )));
+    }
 }
